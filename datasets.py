f35a5b3c20321bd80eaf58b49363e0223a0b03e0
Ubuntu <ubuntu@ip-172-31-32-222.us-west-2.compute.internal>
2022-06-01 05:10:54 +0000

    fisnished dataset

--- datasets.py
+++ datasets.py
@@ -5,22 +5,29 @@ import os
 import json
 from PIL import Image, JpegImagePlugin
 from tqdm import tqdm
+import numpy as np
 
 class COCODataset(torch.utils.data.Dataset):
     def __init__(
             self, imgs_dir, captions_path, transform = None, tgt_img_size = (256, 256)
     ):
         # create list of images directories
-        self.imgs_path = []
-        for img_name in os.listdir(imgs_dir):
-            image = Image.open('{}/{}'.format(imgs_dir, img_name))
-            print(image.shape)
-            self.imgs_path.append('{}/{}'.format(imgs_dir, img_name))
+        with open('imgs_black_white.txt', 'r') as f:
+            blacklist = [c.replace('\n', '') for c in f.readlines()]
+        # print(blacklist)
+        self.imgs_path = ['{}/{}'.format(imgs_dir, img_name) for img_name in os.listdir(imgs_dir) if img_name not in blacklist]
+        # self.imgs_path = []
+        # with open('imgs_black_white.txt', 'w') as f:
+        #     for img_name in os.listdir(imgs_dir):
+        #         image = Image.open('{}/{}'.format(imgs_dir, img_name))
+        #         if len(np.array(image).shape) != 3:
+        #             f.write('%s\n' % img_name)
+        #         self.imgs_path.append('{}/{}'.format(imgs_dir, img_name))
             # ['{}/{}'.format(imgs_dir, img_name) for img_name in os.listdir(imgs_dir)]
 
         # create JSON captions
-        f = open(captions_path)
-        self.captions = json.load(f)
+        with  open(captions_path) as f:
+            self.captions = json.load(f)
 
         # create transform
         if transform is None:
@@ -44,7 +51,7 @@ class COCODataset(torch.utils.data.Dataset):
         return image, torch.zeros(1)
 
 def test():
-    imgs_dir = '/home/ubuntu/datasets/coco/train2017/'
+    imgs_dir = '/home/ubuntu/datasets/coco/train2017'
     captions_path = '/home/ubuntu/datasets/coco/annotations/captions_train2017.json'
     data = COCODataset(
         imgs_dir = imgs_dir, captions_path = captions_path
